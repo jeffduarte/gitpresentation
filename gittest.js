14d4ac8715ccf88c74d988ce1cc771dc0a96e4b2
Jeferson Change <Jefersonchange@gmail.com>
2018-03-21 15:31:11 -0300

    first commit

---
+++ gittest.js
@@ -0,0 +1,13 @@
+add = (a = 1, b = 2) => console.log(a + b);
+
+sub = (a = 1, b = 2) => console.log(a - b);
+
+mult = (a = 1, b = 2) => console.log(a * b);
+
+div = (a = 1, b = 2) => {console.log(a / b)};
+
+div();
+
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+  });
