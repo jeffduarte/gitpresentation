8bbc34ee39165b0633f0a1005e5199c46cb68e01
Jeferson Change <Jefersonchange@gmail.com>
2018-03-21 16:17:27 -0300

    second commit

--- gittest.js
+++ gittest.js
@@ -2,7 +2,11 @@ add = (a = 1, b = 2) => console.log(a + b);
 
 sub = (a = 1, b = 2) => console.log(a - b);
 
-mult = (a = 1, b = 2) => console.log(a * b);
+mult = (a = 1, b = 2) => {
+  
+  console.log("first change");
+  console.log(a * b)
+};
 
 div = (a = 1, b = 2) => {console.log(a / b)};
 
