e896c5356f1069cccbe3986ec0b1a4bf2137e9ce
Jeferson Change <Jefersonchange@gmail.com>
2018-03-22 16:06:52 -0300

    tentando criar conflito

--- gittest.js
+++ gittest.js
@@ -5,10 +5,11 @@ sub = (a = 1, b = 2) => console.log(a - b);
 mult = (a = 1, b = 2) => {
   console.log(a * b);
   console.log("new branch");
-  console.log("merge conflict test");
+  console.log("");
 }
 div = (a = 1, b = 2) => {console.log(a / b)};
 
+div();
 
 process.argv.forEach(function (val, index, array) {
     console.log(index + ': ' + val);
