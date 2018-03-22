db6a480e9d2858228764d33adf1c3c7eb2196f20
Jeferson Change <Jefersonchange@gmail.com>
2018-03-22 15:14:13 -0300

    atual

--- gittest.js
+++ gittest.js
@@ -2,11 +2,7 @@ add = (a = 1, b = 2) => console.log(a + b);
 
 sub = (a = 1, b = 2) => console.log(a - b);
 
-mult = (a = 1, b = 2) => {
-  
-  console.log("first change");
-  console.log(a * b)
-};
+mult = (a = 1, b = 2) => console.log(a * b);
 
 div = (a = 1, b = 2) => {console.log(a / b)};
 
